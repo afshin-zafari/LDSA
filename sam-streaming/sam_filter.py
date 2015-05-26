14650e8f873e0636b0328478af02e719d1bea1b4
Ubuntu <ubuntu@host-10-0-100-230.openstacklocal>
2015-05-26 10:24:11 +0000

    New modification

--- sam-streaming/sam_filter.py
+++ sam-streaming/sam_filter.py
@@ -1,7 +1,7 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 import sys
 for line in sys.stdin:
     fields = line.split("\t")
     #print fields
-    if int(fields[2])==20 and abs(int(fields[8])) >1000: 
+    if int(fields[2])==20 and abs(int(fields[8])) >400: 
        print line
