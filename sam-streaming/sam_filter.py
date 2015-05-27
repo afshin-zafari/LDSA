1a8ebcd2ebbba3aedcb332ed8f57df0c39a6016f
Ubuntu <ubuntu@host-10-0-100-244.openstacklocal>
2015-05-27 09:01:02 +0000

    In Mapper, the '\t' characters in input line (SAM records) should be relaced with space sice '\t' is used for key-value separation.
    In Reducer, it is sufficient to use the Java class IdentityReducer and no reducer.py is needed.
    Since the IdentityReducer takes the value part of the inputs and prints them unchanged, the Mapper output should be set in the value part.
    Therefore, the key-value pair in the Mapper should be :'something'+'\t'+SAM.line. 'something' is used as key for the reducer and will be ignored.
    ----
    .gitignore: The *.class and *.jar files should not be tracked by git.

--- sam-streaming/sam_filter.py
+++ sam-streaming/sam_filter.py
@@ -4,4 +4,4 @@ for line in sys.stdin:
     fields = line.split("\t")
     #print fields
     if int(fields[2])==20 and abs(int(fields[8])) >400: 
-       print line
+       print ' \t'+line.replace('\t',' ')
